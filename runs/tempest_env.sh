608f4879fb849fdfdf7ec2bb441760776b95859f
Haining Wang <hw56@iu.edu>
2026-07-07 18:05:14 -0400

    Fix Tempest module loading

--- runs/tempest_env.sh
+++ runs/tempest_env.sh
@@ -3,7 +3,25 @@
 
 source /usr/local/Modules/5.6.1/init/bash
 module purge
-source /home/g91p721/.module/py312
+
+module use --append /etc/scl/modulefiles
+module use --append /mnt/shared/modulefiles
+module use --append /mnt/shared/ebmodules/eb/all
+module use --append /mnt/global/modulefiles
+
+module load GCCcore/13.3.0
+module load zlib/1.3.1-GCCcore-13.3.0
+module load binutils/2.42-GCCcore-13.3.0
+module load bzip2/1.0.8-GCCcore-13.3.0
+module load ncurses/6.5-GCCcore-13.3.0
+module load libreadline/8.2-GCCcore-13.3.0
+module load Tcl/8.6.14-GCCcore-13.3.0
+module load SQLite/3.45.3-GCCcore-13.3.0
+module load XZ/5.4.5-GCCcore-13.3.0
+module load libffi/3.4.5-GCCcore-13.3.0
+module load OpenSSL
+module load Python/3.12.3-GCCcore-13.3.0
+module load CUDA/12.3.0
 
 export PYTHONNOUSERSITE=1
 export PYTHONUNBUFFERED=1
